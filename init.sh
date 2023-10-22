9c44b38af7d61f07a5a1eea0284475289449e393
RAM <ali_rayhan19@hotmail.com>
2023-10-22 13:54:01 +0700

    update

--- init.sh
+++ init.sh
@@ -5,7 +5,9 @@
 install_nvim() {
   curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
   chmod u+x nvim.appimage
-  ./nvim.appimage
+  ./nvim.appimage --appimage-extract
+  sudo mv squashfs-root /
+  sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 }
 
 # Install NODE JS
