5ef12ff7d722e986308f51479e3b469f47bb6223
RAM <ali_rayhan19@hotmail.com>
2024-04-01 09:00:25 +0700

    Updated lsp

--- nvim/lua/core/lsp.lua
+++ nvim/lua/core/lsp.lua
@@ -1,6 +1,5 @@
 -- Setup language servers.
 local lspconfig = require('lspconfig')
-lspconfig.pyright.setup {}
 lspconfig.tsserver.setup {}
 lspconfig.gopls.setup{}
 lspconfig.golangci_lint_ls.setup{}
@@ -16,10 +15,44 @@ lspconfig.rust_analyzer.setup {
   },
 }
 lspconfig.bashls.setup{}
-lspconfig.pyright.setup{}
--- lspconfig.phpactor.setup{}
+-- lspconfig.phpactor.setup{
+--   filetypes = { "php", "blade" },
+-- }
 lspconfig.intelephense.setup{
-  filetypes = { "php", "blade" },
+  filetypes = { "php" },
+}
+lspconfig.jedi_language_server.setup{}
+lspconfig.vimls.setup{}
+lspconfig.lua_ls.setup{
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+    if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+      return
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT'
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME
+          -- Depending on the usage, you might want to add additional paths here.
+          -- "${3rd}/luv/library"
+          -- "${3rd}/busted/library",
+        }
+        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+        -- library = vim.api.nvim_get_runtime_file("", true)
+      }
+    })
+  end,
+  settings = {
+    Lua = {}
+  }
 }
 
 
